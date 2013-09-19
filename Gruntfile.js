9c18222fc5591c692090871853201d3c1e4f765b
Christophe Rosset <tophe@topheman.com>
2013-09-19 15:01:15 +0200

    change Gruntfile to allow release launch

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
         // grunt-contrib-connect will serve the files of the project
         // on specified port and hostname
         connect: {
-            debug: {
+            all: {
                 options: {
                     port: 9000,
                     hostname: "0.0.0.0",
@@ -22,13 +22,17 @@ module.exports = function(grunt) {
         // grunt-open will open your browser at the project's URL
         open: {
             debug: {
-                path: 'http://localhost:<%= connect.debug.options.port%>/examples/simple.headtrackr.html'
+                path: 'http://localhost:<%= connect.all.options.port%>/examples/simple.headtrackr.html'
+            },
+            release: {
+                path: 'http://localhost:<%= connect.all.options.port%>/'
             }
         }
 
     });
 
-    grunt.registerTask('server', ['open:debug', 'connect:debug']);
-    grunt.registerTask('server-debug', ['open:debug', 'connect:debug']);
+    grunt.registerTask('server', ['open:debug', 'connect:all']);
+    grunt.registerTask('server-debug', ['open:debug', 'connect:all']);
+    grunt.registerTask('server-release', ['open:release', 'connect:all']);
     
 };
